7383ea7fc75074edbfcee061afb76b4cb3e5df40
kuraki <583378006@qq.com>
2019-06-14 14:11:16 +0800

    update

---
+++ chapter16/webtest/my_math.py
@@ -0,0 +1,19 @@
+def square(x):
+    '''
+    计算平方并返回结果
+    >>> square(2)
+    4
+    >>> square(3)
+    9
+    '''
+    return x ** x
+
+
+def product(x, y):
+    return x * y
+
+
+if __name__ == '__main__':
+    import doctest, my_math
+
+    doctest.testmod(my_math)
